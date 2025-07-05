4e27406521a4a76f8c8334fb54df4f73aa8fb6e9
IlyesGml <128257644+IlyesGml@users.noreply.github.com>
2025-07-05 09:11:12 -0400

    re

--- BMS_LOG/Software/STM32-DISC1/BMS_MTP_VB.00/BMS_MTP_VB.00/Core/Processus/processusRS485.c
+++ BMS_LOG/Software/STM32-DISC1/BMS_MTP_VB.00/BMS_MTP_VB.00/Core/Processus/processusRS485.c
@@ -5,6 +5,34 @@
  *      Author: ilyes
  */
 
+// INCLUSIONS
+#include "main.h"
 
+#include "../Service/serviceBaseDeTemps.h"
+#include "processusRS485.h"
 
+// Definitions privees
 
+// Declarations de fonctions privees:
+unsigned int processusRS485_compteur;
+
+// Definitions de variables privees:
+
+// Definitions de fonctions privees:
+
+// --- Fonctions d'initialisation ---
+
+void processusRS485_waitingHandShake(void)
+{
+    // A definir selon le protocole RS485
+}
+
+/** @brief Initialisation @param @retval */
+void processusRS485_initialise(void)
+{
+    processusRS485_compteur = 0;
+    serviceBaseDeTemps_execute[PROCESSUSRS485_PHASE] =
+        processusRS485_waitingHandShake;
+}
+
+// --- Fonctions pour gestion RS485 ---
--- BMS_LOG/Software/STM32-DISC1/BMS_MTP_VB.00/BMS_MTP_VB.00/Core/Processus/processusRS485.h
+++ BMS_LOG/Software/STM32-DISC1/BMS_MTP_VB.00/BMS_MTP_VB.00/Core/Processus/processusRS485.h
@@ -8,9 +8,31 @@
 #ifndef PROCESSUS_PROCESSUSRS485_H_
 #define PROCESSUS_PROCESSUSRS485_H_
 
+//MODULE: processusRS485
+//DESCRIPTION: Implémente la gestion de la communication RS485.
 
+//HISTORIQUE:
+// 2025-06-30, ilyes, création
 
+//DEFINITIONS REQUISES PAR LE MODULE:
+//Dépendances matérielles
+//(copiez et adaptez ce qui suit dans "main.h")
+//pas de dépendances matérielles
 
+//Dépendances logicielles
+//(copiez et adaptez ce qui suit dans "main.h")
+//#define PROCESSUSRS485_BAUDRATE 115200
+//#define PROCESSUSRS485_TIMEOUT_MS 100
+
+//INFORMATION PUBLIQUE:
+//Définitions publiques:
+//pas de définitions publiques
+
+//Fonctions publiques:
+void processusRS485_initialise(void);
+void processusRS485_waitingHandShake(void);
+
+//Variables publiques:
+//pas de variables publiques
 
 #endif /* PROCESSUS_PROCESSUSRS485_H_ */
-
